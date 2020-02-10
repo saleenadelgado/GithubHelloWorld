f323281c2ef9e3c7e5c22ebe5fc24c5dc6f215a3
saleenadelgado <Saleena.delgado.2022@student.lhschools.org>
2020-02-10 12:28:21 -0500

    called Hello World Method again

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -2,10 +2,18 @@ public class GithubHelloWorld {
     public static void main(String[] args) {
 System.out.println("Hello World");
  helloWorld();
+ yourName();
     }
 
     public static void helloWorld(){
+
         System.out.println("Hello GitHub World");
+    }
+
+    
+
+    public static void yourName(){
+        System.out.println("This is Saleena");
     }
 
 
